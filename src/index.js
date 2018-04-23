c6485f5866f7f06988f6a0c9fa4abbbc35925e41
Ryan Orsinger <ryan.orsinger@gmail.com>
2018-04-23 16:55:14 -0500

    add npm intro solution

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const $ = require('jquery');
+
+const sayHello = () => console.log("Hi, everybody!");
+
+sayHello();
+
+$("body").css("background-color", "salmon");
+
+$("body").append("<h1>Happy NPM Day, everybody!</h1>");
